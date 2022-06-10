b37a0a6b432dfdd52ba6738350362b545ae1e5dd
SacchXN <106769040+SacchXN@users.noreply.github.com>
2022-06-11 01:30:57 +0200

    Added more input validation check and a new chart type.

--- chart.py
+++ chart.py
@@ -49,14 +49,14 @@ def get_pulls(log):
 def partial_time(fights, pull_time):
   pt = {}
   for day in pull_time.keys():
-    pt[day] = {phase: round((sum(pull[1] for pull in pull_time[day] if pull[0]==phase)/60)) for phase in fights.keys()}
+    pt[day] = {phase: round(sum(pull[1] for pull in pull_time[day] if pull[0]==phase)) for phase in fights.keys()}
    
   return pt
 
 def total_time(fights, pull_time):
   pt = {}
   for day in pull_time.keys():
-    pt[day] = {phase: round((sum(pull[1] for pull in pull_time[day] if pull[0]==phase)/60)) for phase in fights.keys()}
+    pt[day] = {phase: round(sum(pull[1] for pull in pull_time[day] if pull[0]==phase)) for phase in fights.keys()}
 
   return {phase: sum(pt[day][phase] for day in pt.keys()) for phase in fights.keys()}
 
@@ -85,7 +85,7 @@ def bar_single(fights, day, pulls):
 
 
 def pie_single(fights, day, pull_time):
-  pt_filtered = {k: v for k, v in pull_time.items() if v}
+  pt_filtered = {k: v/60 for k, v in pull_time.items() if v}
   fights_filtered = {k: v for k, v in fights.items() if k in list(pt_filtered.keys())}
  
   
@@ -94,9 +94,38 @@ def pie_single(fights, day, pull_time):
   fig.set_tight_layout(True)
   
   plot.pie(pt_filtered.values(), [0.03]*(len(pt_filtered.keys())),  autopct = lambda p: '{:.0f}%\n{:.0f}m'.format(p,round(sum(list(pt_filtered.values()))*p/100)), 
-           labels = [v[0] for v in fights_filtered.values()], startangle=90, colors = [v[1] for v in fights_filtered.values()], textprops = {'fontsize': 15})
+           labels = [v[0] for v in fights_filtered.values()], startangle=90, colors = [v[1] for v in fights_filtered.values()])
 
 
+  buf = io.BytesIO()
+  fig.savefig(buf)
+  buf.seek(0)
+  fig.clear()
+  plt.close(fig)
+  return buf
+
+def pulls_length(fights, day, pull_time):
+  pt_length = [p[1]/60 for p in pull_time]
+  colors = []
+  for x, y in zip(pt_length[:-1], pt_length[1:]):
+    if x > y:
+      colors.append("red")
+    else:
+      colors.append("green")
+    
+  pull_number = np.arange(1, len(pt_length)+1, 1)
+
+  fig, plot = plt.subplots(1,1, figsize=(max(6.4,len(pt_length)/4,5),5))
+  fig.set_tight_layout(True)
+  for x, y, z in zip(pt_length[:-1], pt_length[1:], np.arange(0, len(pt_length), 1)):
+    plot.plot([pull_number[z], pull_number[z+1]], [x, y], color = colors[z], marker='D', markeredgecolor='black', markerfacecolor='white')
+
+  plot.set_ylabel("Pull length in minutes")
+  plot.set_xlabel(day)
+  plot.xaxis.set_label_coords(0.5, -0.1)
+  plot.set_xticks(pull_number)
+  plot.set_xticklabels(pull_number, rotation=90)
+  
   buf = io.BytesIO()
   fig.savefig(buf)
   buf.seek(0)
@@ -134,14 +163,14 @@ def bar_multi(fights, days, pulls):
   return buf
 
 def pie_multi(fights, pull_time):
-  pt_filtered = {k: v for k, v in pull_time.items() if v}
+  pt_filtered = {k: v/60 for k, v in pull_time.items() if v}
   fights_filtered = {k: v for k, v in fights.items() if k in list(pt_filtered.keys())}
     
   fig, plot = plt.subplots(1,1, figsize=(7,7))
   fig.set_tight_layout(True)
   
   plot.pie(pt_filtered.values(), [0.03]*(len(pt_filtered.keys())),  autopct = lambda p: '{:.0f}%\n{:.0f}m'.format(p,round(sum(list(pt_filtered.values()))*p/100)), 
-           labels = [v[0] for v in fights_filtered.values()], startangle=90, colors = [v[1] for v in fights_filtered.values()], textprops = {'fontsize': 15})
+           labels = [v[0] for v in fights_filtered.values()], startangle=90, colors = [v[1] for v in fights_filtered.values()])
 
 
   buf = io.BytesIO()
@@ -186,42 +215,61 @@ async def plot_s(ctx, arg):
     urls = []
     try:
       for item in arg.split(','):
-          urls.append(item.split('/reports/')[1].split('/')[0])
+        code = (item.split('/reports/')[1].split('/')[0])
+        if len(code) == 16:
+          urls.append(code)
+        else:
+          raise ValueError("The report code has not the correct length or is not valid.")
+    except TypeError as e:
+      print("EXCEPTION Log code not valid: {}".format(e))
+      await ctx.send("Log code not valid")
+    except ValueError as e:
+      print("EXCEPTION Log code length: {}".format(e))
+      await ctx.send(e)
     except Exception as e:
       print("EXCEPTION Log Read: {}".format(e))
       await ctx.send("Wrong input")
-      
-    for item in urls:
-        await ctx.send(item)
-        
+
     logs = {}
     pull_time = {}
-    for item in urls:
-      day, pulls, p_time = get_pulls(item)
-      logs[day] = pulls
-      pull_time[day] = p_time
-      
-    files = []
-    partial_mins = {}
-    total_mins = {}
-    
-    try:
-      for item in list(logs.keys()):
-        files.append(bar_single(fights["dsr"], item, logs[item]))
-    except Exception as e:
-      print("EXCEPTION Bar Single: {}".format(e))
-      await ctx.send("Error occurred during bar single chart printing.")
-      
-    partial_mins = partial_time(fights["dsr"], pull_time)
 
     try:
-      for item in partial_mins.keys():
-        files.append(pie_single(fights["dsr"], item, partial_mins[item]))
-    except Exception as e:
-      print("EXCEPTION Pie Single: {}".format(e))
-      await ctx.send("Error occurred during pie single chart printing.")
+      for item in urls:
+        day, pulls, p_time = get_pulls(item)
+        logs[day] = pulls
+        pull_time[day] = p_time
+    except TypeError as e:
+        print("EXCEPTION Log code not valid: {}".format(e))
+        await ctx.send("Log code not valid")
+    if len(logs.keys()):
+      files = []
+      partial_mins = {}
+      total_mins = {}
 
-    for item in files:
+      try:
+        for item in list(logs.keys()):
+          files.append(bar_single(fights["dsr"], item, logs[item]))
+      except Exception as e:
+        print("EXCEPTION Bar Single: {}".format(e))
+        await ctx.send("Error occurred during bar single chart printing.")
+      
+      partial_mins = partial_time(fights["dsr"], pull_time)
+
+      try:
+        for item in partial_mins.keys():
+          files.append(pie_single(fights["dsr"], item, partial_mins[item]))
+      except Exception as e:
+        print("EXCEPTION Pie Single: {}".format(e))
+        await ctx.send("Error occurred during pie single chart printing.")
+
+      try:
+      for item in pull_time.keys():
+        files.append(pulls_length(fights["dsr"], item, pull_time[item]))
+      except Exception as e:
+        print("EXCEPTION Pulls Length: {}".format(e))
+        await ctx.send("Error occurred during pulls length chart printing.")
+        
+      for item in files:
         await ctx.send(file=discord.File(item, 'picture.png'))
         
 @bot.command()
@@ -230,40 +278,49 @@ async def plot_m(ctx, arg):
     urls = []
     try:
       for item in arg.split(','):
-          urls.append(item.split('/reports/')[1].split('/')[0])
+          code = (item.split('/reports/')[1].split('/')[0])
+          if len(code) == 16:
+            urls.append(code)
+          else:
+            raise ValueError("The report code has not the correct length or is not valid.")
+    except ValueError as e:
+      print("EXCEPTION Log code length: {}".format(e))
+      await ctx.send(e)
     except Exception as e:
       print("EXCEPTION Log Read: {}".format(e))
       await ctx.send("Wrong input")
-      
-    for item in urls:
-        await ctx.send(item)
-        
+
     logs = {}
     pull_time = {}
-    for item in urls:
-      day, pulls, p_time = get_pulls(item)
-      logs[day] = pulls
-      pull_time[day] = p_time
-      
-    files = []
-    partial_mins = {}
-    total_mins = {}
-
     try:
-      files.append(bar_multi(fights["dsr"], list(logs.keys()), logs))
-    except Exception as e:
-      print("EXCEPTION Bar Multi: {}".format(e))
-      await ctx.send("Error occurred during bar multi chart printing.")
+      for item in urls:
+        day, pulls, p_time = get_pulls(item)
+        logs[day] = pulls
+        pull_time[day] = p_time
+    except TypeError as e:
+      print("EXCEPTION Log code not valid: {}".format(e))
+      await ctx.send("Log code not valid")
 
-    total_mins = total_time(fights["dsr"], pull_time)
+    if len(logs.keys()):  
+      files = []
+      partial_mins = {}
+      total_mins = {}
 
-    try:
-      files.append(pie_multi(fights["dsr"], total_mins))
-    except Exception as e:
-      print("EXCEPTION Pie Multi: {}".format(e))
-      await ctx.send("Error occurred during pie multi chart printing.")
+      try:
+        files.append(bar_multi(fights["dsr"], list(logs.keys()), logs))
+      except Exception as e:
+        print("EXCEPTION Bar Multi: {}".format(e))
+        await ctx.send("Error occurred during bar multi chart printing.")
 
-    for item in files:
+      total_mins = total_time(fights["dsr"], pull_time)
+
+      try:
+        files.append(pie_multi(fights["dsr"], total_mins))
+      except Exception as e:
+        print("EXCEPTION Pie Multi: {}".format(e))
+        await ctx.send("Error occurred during pie multi chart printing.")
+
+      for item in files:
         await ctx.send(file=discord.File(item, 'picture.png'))
         
 bot.run(TOKEN)
